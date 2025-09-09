246028e65f99e5e842219c19a737b9a3651f3981
Yunhao Zhang <zhangyunh@gmail.com>
2025-09-08 19:31:26 -0500

    Use spisdcard for tang_nano_20k

--- make.py
+++ make.py
@@ -743,7 +743,7 @@ class Sipeed_tang_nano_20k(Board):
         Board.__init__(self, sipeed_tang_nano_20k.BaseSoC, soc_capabilities={
             # Communication
             "serial",
-            "sdcard",
+            "spisdcard",
         })
 
 # Sipeed Tang Primer 20K support -------------------------------------------------------------------
